56f5abd7331b36e6042f2d371f2ddc1d217c0a60
Uchenna Akahara <akaharauchenna@tamu.edu>
2022-11-08 22:23:26 -0600

    Add buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
+import styled from "styled-components";
+
+
 
 function App() {
-  
+  const Button = styled.button`
+    background-color: blue;
+  `;
+
+  const Container = styled.div``;
   const handleClick = () => {
     //call post command
-    
   };
 
   return (
-    <div className="App">
-    <button type="button" onClick={handleClick}>Subtract Ingredient</button>
+    <div className="Server">
+      <h1>Server</h1>
+      <div className="item_box">
+        <button type="button" onClick={handleClick}>
+          Subtract Ingredient
+        </button>
+        <button>Item 1</button>
+        <button>Item 2</button>
+        <button>Item 3</button>
+        <button>Item 4</button>
+        <button>Item 5</button>
+        <button>Item 6</button>
+        <button>Item 7</button>
+        <button>Item 8</button>
+      </div>
     </div>
   );
 }
---
+++ dbSetup2.java
@@ -0,0 +1,5 @@
+public final class dbSetup2  {
+  //TODO: Update user and pswd
+  public static final String user = "csce331_906_shankar";
+  public static final String pswd = "528003863";
+}
