650a2941811fcf8b1779177cc9a03e322930f99b
aazmir24 <aazmir24@tamu.edu>
2022-11-21 22:18:38 -0600

    added manager front end

---
+++ client/src/pages/Manager.js
@@ -0,0 +1,7 @@
+import { TextField, Button, Button as MuiButton } from "@mui/material";
+import { Textarea } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+import "./Manager.css";
+
